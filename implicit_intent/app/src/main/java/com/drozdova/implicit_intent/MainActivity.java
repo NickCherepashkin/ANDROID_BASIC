4e13d97ee7770a5d0461fa1e58272a453473b8f0
Nika Drozdova <cherepawka13@mail.ru>
2022-06-20 01:12:30 +0300

    add editTexts and Buttons in app design

--- implicit_intent/app/src/main/java/com/drozdova/implicit_intent/MainActivity.java
+++ implicit_intent/app/src/main/java/com/drozdova/implicit_intent/MainActivity.java
@@ -3,6 +3,7 @@ package com.drozdova.implicit_intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+    }
+
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
     }
 }
